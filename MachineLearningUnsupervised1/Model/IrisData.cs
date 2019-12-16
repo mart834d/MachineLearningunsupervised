a890189e75e2e80fc1504842170755d6f2c2f7f5
mart834d <weiz1337@outlook.dk>
2019-12-16 17:44:58 +0100

    first commit

---
+++ MachineLearningUnsupervised1/Model/IrisData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace MachineLearningUnsupervised1.Model
+{
+    class IrisData
+    {
+        [LoadColumn(0)] public float SepalLength;
+        [LoadColumn(1)] public float SepalWidth;
+        [LoadColumn(2)] public float PetalLength;
+        [LoadColumn(3)] public float PetalWidth;
+        [LoadColumn(4)] public string Label;
+    }
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")] public uint PredictedClusterId;
+        [ColumnName("Score")] public float[] Distances;
+    }
+}
---
+++ MachineLearningUnsupervised1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using MachineLearningUnsupervised1.Model;
+using Microsoft.ML;
+
+namespace MachineLearningUnsupervised1
+{
+    class Program
+    {
+        private static string dataPath = Path.Combine(Environment.CurrentDirectory, "iris.csv");
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+
+
+            //import data
+            var trainingData = mlContext.Data.LoadFromTextFile<IrisData>(
+            path: dataPath,
+            hasHeader: false,
+            separatorChar: ',');
+
+            //data preparation and training pipeline
+            var pipeline = mlContext.Transforms
+                .Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans("Features", numberOfClusters: 3));
+
+
+            //train
+            var model = pipeline.Fit(trainingData);
+
+            //prediction
+            var dataToMakePredictionOn = new IrisData() { PetalLength = 0.2f, PetalWidth = 5.6f, SepalLength = 1f, SepalWidth = 1.6f };
+            var label = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model).Predict(dataToMakePredictionOn);
+
+            Console.WriteLine($"{string.Join(" ", label.Distances)}");
+        }
+    }
+}
